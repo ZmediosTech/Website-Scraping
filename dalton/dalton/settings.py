c8cb1d6b14de61898084d8d813d0c8a835cd9b5f
unknown <shyamji.yadav@zmediostech.com>
2025-06-20 00:22:05 -0700

    render config

--- dalton/dalton/settings.py
+++ dalton/dalton/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Quick-start development settings - unsuitable for production
